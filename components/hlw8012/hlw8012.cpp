575d59a049bfdf8794e42eb98f3fcd66dcfd85f9
KaufHA <brian@kaufha.com>
2022-11-26 16:15:44 -0700

    first draft of hlw completed

--- components/hlw8012/hlw8012.cpp
+++ components/hlw8012/hlw8012.cpp
@@ -51,27 +51,14 @@ float HLW8012Component::get_setup_priority() const { return setup_priority::DATA
 
 float HLW8012Component::period_to_power(float period_in) {
     return period_to_hz(period_in) * this->power_multiplier_;
-    // float power = hz * this->power_multiplier_;
-    // ESP_LOGD("KAUF HLW 8012 UPDATE", "CF Valid: %d -- CF LAST: %d -- HZ: %f -- Power: %f", cf_store_.get_valid(), cf_store_.get_last_period(), hz, power);
-    // return (power);
 }
 
 float HLW8012Component::period_to_current(float period_in) {
-    // return period_to_hz(period_in) * this->current_multiplier_;
-    float hz = period_to_hz(period_in);
-    float current = hz * this->current_multiplier_;
-    ESP_LOGD("KAUF HLW 8012 UPDATE", "CF1 Valid: %d -- CF1 LAST: %d -- HZ: %f -- Current: %f", cf1_store_.get_valid(), cf1_store_.get_last_period(), hz, current);
-    return (current);
+    return period_to_hz(period_in) * this->current_multiplier_;
 }
 
 float HLW8012Component::period_to_voltage(float period_in) {
-    // return period_to_hz(period_in) * this->current_multiplier_;
-
-
-    float hz = period_to_hz(period_in);
-    float voltage = hz * this->voltage_multiplier_;
-    ESP_LOGD("KAUF HLW 8012 UPDATE", "CF1 Valid: %d -- CF1 LAST: %d -- HZ: %f -- Current: %f", cf1_store_.get_valid(), cf1_store_.get_last_period(), hz, voltage);
-    return (voltage);
+    return period_to_hz(period_in) * this->voltage_multiplier_;
 }
 
 float HLW8012Component::period_to_hz(float period_in) {
@@ -86,35 +73,35 @@ void HLW8012Component::update() {
     return;
   }
 
+  // execute power block if power sensor exists
   if (this->power_sensor_ != nullptr) {
 
     // if more than 10 seconds since last rising edge, consider power to be zero.  or if no value exists
     if ( ((micros()-cf_store_.get_last_rise()) > 10000000) ||
            std::isnan(this->power_sensor_->state) ) {
-  //    ESP_LOGD("KAUF HLW 8012 UPDATE", "============ hard setting power to zero");
       this->power_sensor_->publish_state(0.0f);
     }
 
     // if more than update interval, publish as if edge occured now (if already zero, stay there)
+    // has to also be greater than last period.  If pulse is taking greater than update interval, need to at least wait that long
+    // before trying to fade down.
     else if ( ((micros()-cf_store_.get_last_rise()) > (this->get_update_interval()*1000)) &&
               ((micros()-cf_store_.get_last_rise()) > cf_store_.get_last_period()) &&
               (this->power_sensor_->state != 0.0f)) {
-    //  ESP_LOGD("KAUF HLW 8012 UPDATE", "============ fading power down -- micros: %d -- last_rise: %d -- interval: %d ", micros(), cf_store_.get_last_rise(), this->get_update_interval());
       this->power_sensor_->publish_state(this->period_to_power(micros()-cf_store_.get_last_rise()));
     }
 
     // otherwise, publish actual value based on last period
     else {
-      //ESP_LOGD("KAUF HLW 8012 UPDATE", "============ doing normal publish");
       this->power_sensor_->publish_state(this->period_to_power(cf_store_.get_last_period()));
     }
 
   }
 
-  // using to block mode change when waiting for reading.  Currently waiting 10 seconds.
+  // used to block mode change when waiting beyond update interval for reading.  Currently timing out at 10 seconds.
   bool dont_change_mode = false;
 
-  // do current if current sensor exists and in current mode
+  // execute current block if current sensor exists and we are in current mode
   if ( (this->current_sensor_ != nullptr) && this->current_mode_) {
 
     // if we don't have a valid period value
@@ -123,7 +110,6 @@ void HLW8012Component::update() {
       // force zero if over 10 seconds
       if ( ((micros()-cf1_store_.get_last_rise()) > 10000000) ||
            std::isnan(this->current_sensor_->state) ) {
-        ESP_LOGD("KAUF HLW 8012 UPDATE", "============ hard setting current to zero");
         this->current_sensor_->publish_state(0.0f);
       }
 
@@ -132,23 +118,20 @@ void HLW8012Component::update() {
                 ((micros()-cf1_store_.get_last_rise()) > cf1_store_.get_last_period()) &&
                 (this->current_sensor_->state != 0.0f) ) {
         this->current_sensor_->publish_state(this->period_to_current(micros()-cf1_store_.get_last_rise()));
-        ESP_LOGD("KAUF HLW 8012 UPDATE", "============ fading down current value");
         dont_change_mode = true;
       } else {
-        // if pulse width sensor does not have a valid value, give at least 10 seconds
+        // if pulse width sensor does not have a valid value, don't change mode to give at least 10 seconds
         dont_change_mode = true;
-        ESP_LOGD("KAUF HLW 8012 UPDATE", "============ staying at zero by default");
       }
     }
     // otherwise, if valid, publish actual value based on last period
     else {
-      ESP_LOGD("KAUF HLW 8012 UPDATE", "============ doing normal current publish");
       this->current_sensor_->publish_state(this->period_to_current(cf1_store_.get_last_period()));
     }
 
   }
 
-  // do voltage if voltage sensor exists and in voltage mode
+  // execute voltage block if voltage sensor exists and we are in voltage mode
   else if ( (this->voltage_sensor_ != nullptr) && !this->current_mode_) {
 
     // if we don't have a valid period value
@@ -157,34 +140,30 @@ void HLW8012Component::update() {
       // force zero if over 10 seconds
       if ( ((micros()-cf1_store_.get_last_rise()) > 10000000) ||
              std::isnan(this->voltage_sensor_->state) ) {
-        ESP_LOGD("KAUF HLW 8012 UPDATE", "============ hard setting voltage to zero");
         this->voltage_sensor_->publish_state(0.0f);
       }
 
       // fade down to zero if less than 10 seconds
       else if ( ((micros()-cf1_store_.get_last_rise()) > (this->get_update_interval()*1000)) &&
                 ((micros()-cf1_store_.get_last_rise()) > cf1_store_.get_last_period()) &&
-                (this->current_sensor_->state != 0.0f) ) {
+                (this->voltage_sensor_->state != 0.0f) ) {
         this->voltage_sensor_->publish_state(this->period_to_voltage(micros()-cf1_store_.get_last_rise()));
-        ESP_LOGD("KAUF HLW 8012 UPDATE", "============ fading down voltage value");
         dont_change_mode = true;
       } else {
-        // if pulse width sensor does not have a valid value, give at least 10 seconds
+        // if pulse width sensor does not have a valid value, don't change mode to give at least 10 seconds
         dont_change_mode = true;
-        ESP_LOGD("KAUF HLW 8012 UPDATE", "============ staying at zero by default");
       }
     }
     // otherwise, if valid, publish actual value based on last period
     else {
-      ESP_LOGD("KAUF HLW 8012 UPDATE", "============ doing normal voltage publish");
       this->voltage_sensor_->publish_state(this->period_to_voltage(cf1_store_.get_last_period()));
     }
 
   }
-  // if we get here, means current mode variable indicates a sensor that doesn't exist.
+  // if we get here, mode variable indicates a sensor that doesn't exist.
   else {
-    ESP_LOGD("KAUF HLW 8012 UPDATE", "============ GOT TO ELSE");
     this->change_mode();
+    dont_change_mode = true;
   }
 
   if ( (this->change_mode_at_++ >= this->change_mode_every_) && !dont_change_mode ) {
@@ -195,8 +174,8 @@ void HLW8012Component::update() {
 
 void HLW8012Component::change_mode() {
     this->current_mode_ = !this->current_mode_;
-    ESP_LOGD(TAG, "Changing mode to %s mode", this->current_mode_ ? "CURRENT" : "VOLTAGE");
-    this->change_mode_at_ = 0;
+    ESP_LOGV(TAG, "Changing mode to %s mode", this->current_mode_ ? "CURRENT" : "VOLTAGE");
+    this->change_mode_at_ = 1;
     this->sel_pin_->digital_write(this->current_mode_);
     this->cf1_store_.reset();
 }
